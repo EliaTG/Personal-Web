38e84afddc8b97f07d4cdd5781f612810b05be7a
Elia <Elitoribio01@gmail.com>
2020-12-16 19:58:13 -0400

    Mi primer commit

---
+++ JS/cool.js
@@ -0,0 +1,10 @@
+const Hambuger = document.querySelector(".hambuger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+Hambuger.addEventListener("click", () => {
+navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
